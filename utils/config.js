859c1ea5815ddd260259fa8a8ff31b326312b174
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-09-24 00:02:58 +0800

    feat 增加分支

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   github: 'xinghaiyang1994/sea-koa',
-  branchs: ['couple', 'api', 'api-login-session', 'api-login-token']
+  branchs: ['couple', 'api', 'api-login-session', 'api-login-token', 'api-session-redis']
 }
